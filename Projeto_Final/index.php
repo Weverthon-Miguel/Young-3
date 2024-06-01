c9dbe9dd2c2eaf17da9e30e6b79588655328b0c1
Weverthon-Miguel <136903060+Weverthon-Miguel@users.noreply.github.com>
2024-06-01 09:56:10 -0300

    Projeto Final

---
+++ Projeto_Final/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="Script.js" defer></script>
+</head>
+<body>
+    <section class="painel">
+        <h1>Confira os Módulos</h1>
+        <form action=""></form>
+        <div class="user">
+            <label id="lnome" for="">Nome</label>
+            <input type="text" name="nome" id="nome">
+        </div>
+        <div class="modulo">
+            <label id="lmodulo" for="">Módulo</label>
+            <input type="text" name="modulo" id="modulo">
+        </div>
+        <div >
+            <button type="submit"  id="bconfirmar">Confirmar</button>
+            <button type="reset" id="blimpar">Limpar</button>
+        </div>
+    </section>
+</body>
+</html>
